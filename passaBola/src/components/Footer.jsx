1cf866a2934a2adb40cd8a31999d578418f73654
Laboratório FIAP <labsfiap@fiap.com.br>
2025-11-06 09:34:08 -0300

    corrigindo o footer

--- passaBola/src/components/Footer.jsx
+++ passaBola/src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <>
       <footer className="bg-gray-900 text-white py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="grid md:grid-cols-5 gap-8 ">
+          <div className="md:grid md:grid-cols-5 gap-8 ">
             <div className="col-start-2 col-end-2">
               <div className="flex items-center space-x-2 mb-6">
                 <div className="w-10 h-10 bg-pink-500 rounded-full flex items-center justify-center">
